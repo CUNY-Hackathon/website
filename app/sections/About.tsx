d5a46aed7cdd68675239fb173712a98bdfbf082e
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-11-24 13:07:40 -0500

    some fixes

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -16,18 +16,18 @@ export default function About() {
         <p className="text-xl w-[50%] leading-8">
           Welcome to HackCUNY, an annual hackathon exclusively designed for the
           vibrant and diverse student body of the City University of New York
-          (CUNY). Our event is more than just a competition; it's a celebration
-          of innovation, creativity, and collaboration. Each year, we invite
-          CUNY students from all backgrounds and skill levels to come together
-          for an exhilarating 48-hour journey of hacking. At HackCUNY, you'll
-          get the chance to turn your ideas into reality, learn new
+          (CUNY). Our event is more than just a competition; it&apos;s a
+          celebration of innovation, creativity, and collaboration. Each year,
+          we invite CUNY students from all backgrounds and skill levels to come
+          together for an exhilarating 48-hour journey of hacking. At HackCUNY,
+          you&apos;ll get the chance to turn your ideas into reality, learn new
           technologies, and connect with fellow students and industry leaders.
-          Whether you're a seasoned coder, a design enthusiast, or a beginner
-          with a passion for problem-solving, HackCUNY is the perfect platform
-          to challenge yourself, expand your network, and showcase your talents.
-          Join us in fostering a community of tech-driven minds, united by the
-          spirit of discovery and a shared ambition to drive positive change
-          through technology.
+          Whether you&apos;re a seasoned coder, a design enthusiast, or a
+          beginner with a passion for problem-solving, HackCUNY is the perfect
+          platform to challenge yourself, expand your network, and showcase your
+          talents. Join us in fostering a community of tech-driven minds, united
+          by the spirit of discovery and a shared ambition to drive positive
+          change through technology.
         </p>
         <Image
           src="/hackathon_logo.png"
